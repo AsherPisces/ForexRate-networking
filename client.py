8a30df1c21947a02d0cf56afab938137d358d9e4
Nguyễn Thái Hiệp <75217504+thaihiep232002@users.noreply.github.com>
2021-07-20 16:10:33 +0700

    Add files via upload

--- client.py
+++ client.py
@@ -1,12 +1,23 @@
 import socket
+# Hello anh =)))
 
-SERVER_HOST = "192.168.1.119"
-SERVER_PORT = 5050
+SERVER_HOST = "192.168.1.77"
+SERVER_PORT = 5656
+
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 client.connect((SERVER_HOST,SERVER_PORT))
+while True:
+    data_client = input("Client: ")
+    client.sendall(data_client.encode("utf-8"))
+    if data_client == "quit":
+        client.close()
+        break
+    data_server = client.recv(1024).decode("utf-8")
+    print(f"Server: {data_server}")
+    if data_server == "quit":
+        client.close()
+        break
 
-data = input("input data: ")
 
-client.sendall(data.encode("utf-8"))
---
+++ server.py
@@ -0,0 +1,28 @@
+# 1 client
+import socket
+PORT = 5656
+HOST = "192.168.1.77"
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server.bind((HOST, PORT))
+server.listen()
+print("Waiting for client: ")
+conn, addr = server.accept()
+print("connect by {}".format(addr))
+print("Hello {}!".format(addr))
+while True:
+    data_client = conn.recv(1024).decode("utf-8")
+    print("Client: {}".format(data_client))
+    if data_client == "quit":
+        conn.close()
+        server.close()
+        break
+    data_server = input("Server: ") 
+    conn.sendall(data_server.encode("utf-8"))
+    if data_server == "quit":
+        conn.close()
+
+
+def login():
+
