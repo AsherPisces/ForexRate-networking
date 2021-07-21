5b43cce47c26b9838a694ec005c0c3ee9e95c0e0
Nguyễn Thái Hiệp <75217504+thaihiep232002@users.noreply.github.com>
2021-07-21 08:59:19 +0700

    Add files via upload

--- client.py
+++ client.py
@@ -1,13 +1,57 @@
 import socket
-# Hello anh =)))
-
-SERVER_HOST = "192.168.1.77"
-SERVER_PORT = 5656
-
+from os import system, name
+def clear():
+    # for windows
+    if name == 'nt':
+        _ = system('cls')
+  
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = system('clear')
+SERVER_PORT = 5446
+SERVER_HOST = "127.0.0.1"
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 client.connect((SERVER_HOST,SERVER_PORT))
+if client.recv(1024).decode("utf-8") == "accepted":
+    print(f"connected with {SERVER_HOST}")
+
+while True:
+    is_success = False
+    print("1. Login: ")
+    print("2. Regis: ")
+    check = input("Select: ")
+    if check == "1":#select login
+        # login
+        while True:
+            # clear() # clear screen
+            client.sendall("login".encode("utf-8"))
+            username = input("Username: ")
+            client.sendall(username.encode("utf-8"))
+            password = input("Password: ")
+            client.sendall(password.encode("utf-8"))
+            if client.recv(1024).decode("utf-8") == "login_success":
+                print("logged success!")
+                is_success = True
+                break
+            else:
+                print("fail success!")
+    elif check == "2":#select registered
+        # regis
+        while True:
+            clear()
+            client.sendall("regis".encode("utf-8"))
+            username = input("Username: ")
+            client.sendall(username.encode("utf-8"))
+            password = input("Password: ")
+            client.sendall(password.encode("utf-8"))
+            if client.recv(1024).decode("utf-8") == "regis_success":
+                print("registered success!")
+                break
+    if is_success == True:
+        break
+
 while True:
     data_client = input("Client: ")
     client.sendall(data_client.encode("utf-8"))
@@ -19,5 +63,8 @@ while True:
     if data_server == "quit":
         client.close()
         break
+
+    
+    
 
 
--- server.py
+++ server.py
@@ -1,19 +1,65 @@
-# 1 client
 import socket
-PORT = 5656
-HOST = "192.168.1.77"
+import threading
+PORT = 5446
+HOST = "127.0.0.1"
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+
 server.bind((HOST, PORT))
 server.listen()
+
 print("Waiting for client: ")
 conn, addr = server.accept()
-print("connect by {}".format(addr))
-print("Hello {}!".format(addr))
+conn.sendall("accepted".encode("utf-8"))
+print(f"connected by {addr}")
+
+def is_logged(username_client, password_client):
+    file_user = open("data_username.txt", "r")
+    file_pass = open("data_password.txt", "r")
+    while True:
+        check_user = file_user.readline().replace("\n","")
+        check_pass = file_pass.readline().replace("\n","")
+        if check_user == username_client and check_pass == password_client:
+            file_user.close()
+            file_pass.close()
+            return True
+        if file_user.readline() == "" or file_pass.readline() == "":
+            file_user.close()
+            file_pass.close()
+            return False
+
+def regis(username_client, password_client):
+    file_user = open("data_username.txt", "w+")
+    file_pass = open("data_password.txt", "w+")
+    file_user.write(username_client + "\n")
+    file_pass.write(password_client + "\n")
+    file_user.close()
+    file_pass.close()
+
+while True:
+    client_select = conn.recv(1024).decode("utf-8")
+    username_client = conn.recv(1024).decode("utf-8")
+    password_client = conn.recv(1024).decode("utf-8")
+    is_success = False
+    if client_select == "login":
+        # login
+        if is_logged(username_client, password_client):
+            print(f"{addr} logged!")
+            conn.sendall("login_success".encode("utf-8"))
+            is_success = True
+        else:
+            conn.sendall("login_fail".encode("utf-8"))
+    if client_select == "regis":
+        # regis
+        regis(username_client, password_client)
+        conn.sendall("regis_success".encode("utf-8"))
+    if is_success == True:
+        break
+
 while True:
     data_client = conn.recv(1024).decode("utf-8")
-    print("Client: {}".format(data_client))
+    print(f"Client: {data_client}")
     if data_client == "quit":
         conn.close()
         server.close()
@@ -22,5 +68,4 @@ while True:
     conn.sendall(data_server.encode("utf-8"))
     if data_server == "quit":
         conn.close()
-
-
+        break
