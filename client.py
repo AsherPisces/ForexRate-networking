0e0d43768379a699da951d857ebeda7675fe0b7f
Nguyễn Thái Hiệp <thaihiep232002@gmail.com>
2021-07-28 00:44:39 +0700

    Add files via upload

--- client.py
+++ client.py
@@ -2,6 +2,7 @@ import socket
 import json
 from threading import Thread
 from os import system, name
+from tkinter import *
 def clear():
     # for windows
     if name == 'nt':
@@ -20,73 +21,118 @@ client.connect((SERVER_HOST,SERVER_PORT))
 welcome = client.recv(1024).decode("utf-8")
 print(welcome)
 
+window = Tk()
+
+window.geometry("500x400")
+window.title("VietNam Coin App")
 user = {}
 
-while True:
-    is_success = False
-    print("1. Login: ")
-    print("2. Regis: ")
-    check = input("Select: ")
-    if check == "1":#select login
-        # login
-        while True:
-            # clear() # clear screen
-            client.sendall("login".encode("utf-8"))
-            user["name"] = input("Username: ")
-            user["password"] = input("Password: ")
-            client.sendall(json.dumps(user).encode("utf-8"))
-            if client.recv(1024).decode("utf-8") == "login_success":
-                print("logged success!")
-                is_success = True
-                break
-            else:
-                print("fail success!")
-    elif check == "2":#select registered
-        # regis
-        while True:
-            # clear()
-            client.sendall("sign_up".encode("utf-8"))
-            user["name"] = input("Username: ")
-            user["password"] = input("Password: ")
-            client.sendall(json.dumps(user).encode("utf-8"))
-            if client.recv(1024).decode("utf-8") == "sign_up_success":
-                print("signed up success!")
-                break
-            else:
-                print("fail success!")
-    if is_success == True:
-        break
+
+def Accept_login(user):
+    client.sendall(json.dumps(user).encode("utf-8"))
+    if client.recv(1024).decode("utf-8") == "login_success":
+        return True
+    else:
+        return False
+        
+def Handle_login(user, user_entry, pass_entry):
+    client.sendall("login".encode("utf-8"))
+    user["name"] = user_entry.get()
+    user["password"] = pass_entry.get()
+    print(user["name"])
+    print(user["password"])
+    if Accept_login(user):
+        Start()
+        print("Login successful")
+    else:
+        label_login = Label(window, text="Login failed!")
+        label_login.grid(row =5, column =1)
+        print("Login failed")
+
+def Accept_signup(user):
+    client.sendall(json.dumps(user).encode("utf-8"))
+    if client.recv(1024).decode("utf-8") == "sign_up_success":
+        return True
+    else:
+        return False
+
+def Handle_signup(user, user_entry, pass_entry):
+    client.sendall("sign_up".encode("utf-8"))
+    user["name"] = user_entry.get()
+    user["password"] = pass_entry.get()
+    if Accept_signup(user):
+        label_signup = Label(window, text="Sign Up successful!")
+        label_signup.grid(row=5, column = 1)
+        print("Sign Up successful")
+    else:
+        label_login = Label(window, text="Sign Up failed!")
+        label_login.grid(row =5, column =1)
+        print("Sign Up failed")
+
+def Start():
+    print("Start")
+    # is_success = False
+    # print("1. Login: ")
+    # print("2. Regis: ")
+
+user_label = Label(window, text = "Username :")
+user_label.grid(row = 1, column = 1)
+user_entry = Entry(window, width = 20)
+user_entry.grid(row = 1, column = 2)
+pass_label = Label(window, text = "Password :")
+pass_label.grid(row = 2, column = 1)
+pass_entry = Entry(window,width = 20)
+pass_entry.grid(row = 2, column = 2)
+login_button = Button(window,
+    text="Log In",
+    command = lambda: (
+    Handle_login(user, user_entry, pass_entry)
+    )
+)
+login_button.grid(row = 3, column = 1)
+signup_button = Button(window,
+    text="Sign Up",
+    command = lambda: (
+    Handle_signup(user, user_entry, pass_entry)
+    )
+)
+signup_button.grid(row = 4, column = 1)
+
+
+
 # perform the work
-print("1. Task A: ")#tra cuu theo ngay
-print("2. Task B: ")
-print("3. Task C: ")
-print("4. Task D: ")
-print("5. Task E: ")
-print("Quit to Exit!")
-# ...more reqest
-while True:
-    user["msg"] = input("> ")
-    if user["msg"] == "1":
-        # do something
-        client.sendall(json.dumps(user).encode("utf-8"))
-    if user["msg"] == "2":
-        # do something
-        client.sendall(json.dumps(user).encode("utf-8"))
-    if user["msg"] == "3":
-        # do something
-        client.sendall(json.dumps(user).encode("utf-8"))
-    if user["msg"] == "4":
-        # do something
-        client.sendall(json.dumps(user).encode("utf-8"))
-    if user["msg"] == "5":
-        # do something
-        client.sendall(json.dumps(user).encode("utf-8"))
-    if user["msg"] == "quit":
-        client.sendall(json.dumps(user).encode("utf-8"))
-    data_server = client.recv(1024).decode("utf-8")
-    print(data_server)
-    if data_server == "quit":
-        break
+# print("1. Task A: ")#tra cuu theo ngay
+# print("2. Task B: ")
+# print("3. Task C: ")
+# print("4. Task D: ")
+# print("5. Task E: ")
+# print("Quit to Exit!")
+# # ...more reqest
+# while True:
+#     user["msg"] = input("> ")
+#     if user["msg"] == "1":
+#         # do something
+#         client.sendall(json.dumps(user).encode("utf-8"))
+#     if user["msg"] == "2":
+#         # do something
+#         client.sendall(json.dumps(user).encode("utf-8"))
+#     if user["msg"] == "3":
+#         # do something
+#         client.sendall(json.dumps(user).encode("utf-8"))
+#     if user["msg"] == "4":
+#         # do something
+#         client.sendall(json.dumps(user).encode("utf-8"))
+#     if user["msg"] == "5":
+#         # do something
+#         client.sendall(json.dumps(user).encode("utf-8"))
+#     if user["msg"] == "quit":
+#         client.sendall(json.dumps(user).encode("utf-8"))
+#     data_server = client.recv(1024).decode("utf-8")
+#     print(data_server)
+#     if data_server == "quit":
+#         break
+
+window.mainloop()
 
 client.close()
 
--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import socket
 import json
 from threading import Thread
 from logger import is_logged, is_sign_up
-PORT = 5457
+PORT = 5455
 HOST = "127.0.0.1"
 # ThreadCount = 0 
 
@@ -10,10 +10,11 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 server.bind((HOST, PORT))
 
-def handle_client(conn, addr):
+def Handle_client(conn, addr):
     # log
     while True:
         client_select = conn.recv(1024).decode("utf-8")
+        print(client_select)
         user_json = conn.recv(1024).decode("utf-8")
         user = json.loads(user_json)
         is_success = False
@@ -34,44 +35,45 @@ def handle_client(conn, addr):
                 conn.sendall("sign_up_fail".encode("utf-8"))
         if is_success == True:
             break
-    clients[user["name"]] = conn
+    
+    # clients[user["name"]] = conn
     # run
-    while True:
-        data_client_json = clients[user["name"]].recv(1024).decode("utf-8")
-        data_client = json.loads(data_client_json)
-        print("{}: {}".format(data_client["name"], data_client["msg"]))
-        if data_client["msg"] == "quit":
-            clients[user["name"]].sendall("quit".encode("utf-8"))
-            clients[user["name"]].close()
-            break
-        # response
+    # while True:
+    #     data_client_json = clients[user["name"]].recv(1024).decode("utf-8")
+    #     data_client = json.loads(data_client_json)
+    #     print("{}: {}".format(data_client["name"], data_client["msg"]))
+    #     if data_client["msg"] == "quit":
+    #         clients[user["name"]].sendall("quit".encode("utf-8"))
+    #         clients[user["name"]].close()
+    #         break
+    #     # response
 
-        if data_client["msg"] == "1":
-            print("{}: reqest task A".format(data_client["name"]))
-            # do something
-            clients[user["name"]].sendall("task A. Done!".encode("utf-8"))
-        if data_client["msg"] == "2":
-            print("{}: reqest task B".format(data_client["name"]))
-            # do something
-            clients[user["name"]].sendall("task B. Done!".encode("utf-8"))
-        if data_client["msg"] == "3":
-            print("{}: reqest task C".format(data_client["name"]))
-            # do something
-            clients[user["name"]].sendall("task C. Done!".encode("utf-8"))
-        if data_client["msg"] == "4":
-            print("{}: reqest task D".format(data_client["name"]))
-            # do something
-            clients[user["name"]].sendall("task D. Done!".encode("utf-8"))
-        if data_client["msg"] == "5":
-            print("{}: reqest task E".format(data_client["name"]))
-            # do something
-            clients[user["name"]].sendall("task E. Done!".encode("utf-8"))
+    #     if data_client["msg"] == "1":
+    #         print("{}: reqest task A".format(data_client["name"]))
+    #         # do something
+    #         clients[user["name"]].sendall("task A. Done!".encode("utf-8"))
+    #     if data_client["msg"] == "2":
+    #         print("{}: reqest task B".format(data_client["name"]))
+    #         # do something
+    #         clients[user["name"]].sendall("task B. Done!".encode("utf-8"))
+    #     if data_client["msg"] == "3":
+    #         print("{}: reqest task C".format(data_client["name"]))
+    #         # do something
+    #         clients[user["name"]].sendall("task C. Done!".encode("utf-8"))
+    #     if data_client["msg"] == "4":
+    #         print("{}: reqest task D".format(data_client["name"]))
+    #         # do something
+    #         clients[user["name"]].sendall("task D. Done!".encode("utf-8"))
+    #     if data_client["msg"] == "5":
+    #         print("{}: reqest task E".format(data_client["name"]))
+    #         # do something
+    #         clients[user["name"]].sendall("task E. Done!".encode("utf-8"))
         # data_server = input("Server: z") 
         # clients[user["name"]].sendall("Accept".encode("utf-8"))
         # if data_server == "quit":
         #     conn.close()
         #     break
-def accept_connection():
+def Accept_connection():
     while True:
         conn, addr = server.accept()
         print(f"connected by {addr}")
@@ -79,14 +81,14 @@ def accept_connection():
         # start_new_thread(handle_client, (conn,addr))
         # ThreadCount += 1
         conn.sendall(f"Server: Welcome {addr}".encode("utf-8"))
-        Thread(target=handle_client, args=(conn, addr)).start()
+        Thread(target=Handle_client, args=(conn, addr)).start()
 
 if __name__ == "__main__":
     # ...the same as hash table
     clients = {}
     server.listen(6)
     print("Waiting for client...")
-    ACCEPT_THREAD = Thread(target=accept_connection)
+    ACCEPT_THREAD = Thread(target=Accept_connection)
     
     ACCEPT_THREAD.start()
     ACCEPT_THREAD.join()
